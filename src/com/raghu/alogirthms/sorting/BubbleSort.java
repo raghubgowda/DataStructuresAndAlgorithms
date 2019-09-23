f8f0b0a761487d379844dfb54ed5be141ff2208d
Raghavendra M Boregowda <raghu.sjce@gmail.com[alias]>
2019-09-23 23:31:39 +0200

    Recursive bubble sort

---
+++ src/com/raghu/alogirthms/sorting/BubbleSort.java
@@ -0,0 +1,31 @@
+package com.raghu.alogirthms.sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    private static void bubbleSort(int[] input, int n) {
+        if (n == 1) {
+            return;
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            if (input[i] > input[i + 1]) {
+                int temp = input[i];
+                input[i] = input[i + 1];
+                input[i + 1] = temp;
+            }
+        }
+        bubbleSort(input, n - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] input = {23, 45, 11, 20, 7, 65, 2};
+        System.out.print("Input : ");
+        System.out.println(Arrays.toString(input));
+
+        bubbleSort(input, input.length);
+
+        System.out.print("Sorted array : ");
+        System.out.println(Arrays.toString(input));
+    }
+}
